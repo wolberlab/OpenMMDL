901e6c904ecf8eee86f5192431ae64ea4533ab21
Valerij Talagayev <v.talagayev@fu-berlin.de>
2022-11-24 20:56:54 +0100

    Fixed mdtraj_conversion Output

--- md_simulation/post_md_conversions.py
+++ md_simulation/post_md_conversions.py
@@ -30,9 +30,9 @@ def mdtraj_conversion(pdb_file):
     """    
     mdtraj_frames = md.load(pdb_file)
     mdtraj_frames.image_molecules()
-    mdtraj_frames.save_dcd(f'centered_old_coord.dcd')
+    mdtraj_frames.save_dcd(f'centered_old_coordinates.dcd')
     mdtraj_first_frame = mdtraj_frames[0:1]
-    mdtraj_first_frame.save_pdb(f'centered_old_coord.pdb')
+    mdtraj_first_frame.save_pdb(f'centered_old_coorinates.pdb')
     
 def MDanalysis_conversion(post_mdtraj_pdb_file, post_mdtraj_dcd_file, ligand_name):
     """
