9a1c5e0d40094667298a65b9c16c73b2689f6421
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2023-06-05 23:43:07 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+autodoc_mock_imports = ["openmmdl"]
+
 
 
 # -- Project information -----------------------------------------------------
