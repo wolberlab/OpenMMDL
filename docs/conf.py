414481eca294a3c648c7e84aa7514609892e52e2
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2023-06-15 16:54:08 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-autodoc_mock_imports = ["cleaning_procedures"]
+import openmmdl_simulation
 
 
 # -- Project information -----------------------------------------------------
