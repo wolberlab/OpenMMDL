971fe3087e986bf910c8dc249c2a1501857c5477
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2023-11-27 15:01:40 +0100

    Update preprocessing.py
    added parser functions

--- openmmdl/openmmdl_analysis/preprocessing.py
+++ openmmdl/openmmdl_analysis/preprocessing.py
@@ -1,6 +1,7 @@
 import MDAnalysis as mda
 import subprocess
 import os
+import re
 
 def process_pdb_file(input_pdb_filename):
     """Process a PDB file to make it compatible with the openmmdl_analysis package.
@@ -61,3 +62,90 @@ def convert_pdb_to_sdf(input_pdb_filename, output_sdf_filename):
     except subprocess.CalledProcessError as e:
         print(f"Error converting PDB to SDF: {e}")
         return
+
+def renumber_atoms_in_residues(input_pdb_file, output_pdb_file, lig_name):
+    # Read the input PDB file
+    with open(input_pdb_file, 'r') as f:
+        pdb_lines = f.readlines()
+
+    new_pdb_lines = []
+    lig_residue_elements = {}
+
+    for line in pdb_lines:
+        if line.startswith("ATOM"):
+            # Extract information from the ATOM line
+            atom_serial = int(line[6:11])
+            atom_name = line[12:16].strip()
+            residue_name = line[17:20].strip()
+
+            # Check if the residue is residue_name
+            if residue_name == lig_name:
+                # Extract the element from the atom name (assuming it starts with a capital letter)
+                element_match = re.match(r"([A-Z]+)", atom_name)
+                if element_match:
+                    element = element_match.group(1)
+                else:
+                    element = atom_name
+
+                # Increment the count for the current element in the current residue_name residue
+                lig_residue_elements[element] = lig_residue_elements.get(element, 0) + 1
+
+                # Update the atom name based on the element count
+                new_atom_name = f"{element}{lig_residue_elements[element]}"
+
+                # Replace the old atom name with the new one
+                line = line[:12] + f"{new_atom_name:4}" + line[16:]
+
+        # Append the line to the new PDB lines
+        new_pdb_lines.append(line)
+
+    # Write the modified PDB lines to the output file
+    with open(output_pdb_file, 'w') as f:
+        f.writelines(new_pdb_lines)
+
+
+def replace_atom_type(data):
+    lines = data.split('\n')
+    for i, line in enumerate(lines):
+        if ' LIG  X' in line:
+            # Extract the last column which contains the atom type (O/N/H)
+            atom_type = line[12:13].strip()
+            # Replace 'X' with the correct atom type
+            lines[i] = line.replace(' LIG  X', f' LIG  {atom_type}')
+    return '\n'.join(lines)
+
+def process_pdb(input_file, output_file):
+    with open(input_file, 'r') as f:
+        pdb_data = f.read()
+
+    modified_data = replace_atom_type(pdb_data)
+
+    with open(output_file, 'w') as f:
+        f.write(modified_data)
+
+def move_hydrogens_to_end(structure, target_residue_name):
+    # Counter for atom numbering within each residue
+    atom_counter = 1
+
+    # Iterate over all models in the structure
+    for model in structure:
+        # Iterate over all chains in the model
+        for chain in model:
+            # Iterate over all residues in the chain
+            for residue in chain:
+                # Check if the residue name matches the target residue name
+                if residue.resname == target_residue_name:
+                    # Collect hydrogen atoms in the residue
+                    hydrogen_atoms = [atom for atom in residue if atom.element == 'H']
+
+                    # Remove hydrogen atoms from the residue
+                    for hydrogen_atom in hydrogen_atoms:
+                        residue.detach_child(hydrogen_atom.id)
+
+                    # Add hydrogen atoms to the end of the residue with renumbering
+                    for hydrogen_atom in hydrogen_atoms:
+                        hydrogen_atom.serial_number = atom_counter
+                        atom_counter += 1
+                        # Change the residue name to avoid conflicts
+                        hydrogen_atom.name = (f'H{atom_counter}')
+                        residue.add(hydrogen_atom)
