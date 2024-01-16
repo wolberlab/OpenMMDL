5ba1e3cf3d6ae4834dd8b7b07d45378dd4290202
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2024-01-16 18:38:47 +0100

    Update test_rdkit_figure_generation.py

--- openmmdl/tests/openmmdl_analysis/test_rdkit_figure_generation.py
+++ openmmdl/tests/openmmdl_analysis/test_rdkit_figure_generation.py
@@ -7,8 +7,14 @@ from pathlib import Path
 from openmmdl.openmmdl_analysis.rdkit_figure_generation import split_interaction_data, highlight_numbers, update_dict, create_and_merge_images, arranged_figure_generation, generate_interaction_dict
 
 test_data_directory = Path("openmmdl/tests/data/openmmdl_analysis/rdkit_figure_generation")
+test_data_directory_files = Path("openmmdl/tests/data/in")
+lig_no_h = test_data_directory_files / 'lig_no_h.pdb'
+complex = test_data_directory_files / 'complex.pdb'
 current_directory = os.getcwd() 
 output_path = 'all_binding_modes_arranged.png'
+
+shutil.copy(str(lig_no_h), '.')
+shutil.copy(str(complex), '.')
 
 @pytest.mark.parametrize("input_data, expected_output", [
     (["60GLUA_4206_4207_4216_4217_4218_4205_hydrophobic"], ['60GLUA 4206 4207 4216 4217 4218 4205 hydrophobic']),
@@ -19,39 +25,39 @@ def test_split_interaction_data(input_data, expected_output):
     result = split_interaction_data(input_data)
     assert result == expected_output
 
-#def test_highlight_numbers():
-#    # Input data
-#    split_data = [
-#        "163GLYA 4202 Acceptor hbond",
-#        "165ASPA 4203 Donor hbond",
-#        "165ASPA 4222 Donor hbond",
-#        "165ASPA 4203 Acceptor hbond",
-#        "125TYRA 4192 Acceptor waterbridge",
-#        "165ASPA 4222 Donor waterbridge",
-#        "161PHEA 4211 4212 4213 4214 4215 4210 hydrophobic",
-#        "59ARGA 4205 4206 4207 4216 4217 4218 Aromatic pication",
-#        "155PHEA 4205 4206 4207 4216 4217 4218 pistacking",
-#        "59ARGA 4194 F halogen",
-#        "166ARGA 4202,4203 Carboxylate NI saltbridge",
-#        "165ASPA 4202 Amine PI saltbridge"
-#    ]
+def test_highlight_numbers():
+    # Input data
+    split_data = [
+        "163GLYA 4202 Acceptor hbond",
+        "165ASPA 4203 Donor hbond",
+        "165ASPA 4222 Donor hbond",
+        "165ASPA 4203 Acceptor hbond",
+        "125TYRA 4192 Acceptor waterbridge",
+        "165ASPA 4222 Donor waterbridge",
+        "161PHEA 4211 4212 4213 4214 4215 4210 hydrophobic",
+        "59ARGA 4205 4206 4207 4216 4217 4218 Aromatic pication",
+        "155PHEA 4205 4206 4207 4216 4217 4218 pistacking",
+        "59ARGA 4194 F halogen",
+        "166ARGA 4202,4203 Carboxylate NI saltbridge",
+        "165ASPA 4202 Amine PI saltbridge"
+    ]
 
-#    starting_idx = 1  # Updated starting index
+    starting_idx = 1  # Updated starting index
 
-#    result = highlight_numbers(split_data, starting_idx)
+    result = highlight_numbers(split_data, starting_idx)
 
-#    highlighted_hbond_donor, highlighted_hbond_acceptor, highlighted_hbond_both, \
-#    highlighted_hydrophobic, highlighted_waterbridge, highlighted_pistacking, highlighted_halogen, \
-#    highlighted_ni, highlighted_pi, highlighted_pication, highlighted_metal = result
+    highlighted_hbond_donor, highlighted_hbond_acceptor, highlighted_hbond_both, \
+    highlighted_hydrophobic, highlighted_waterbridge, highlighted_pistacking, highlighted_halogen, \
+    highlighted_ni, highlighted_pi, highlighted_pication, highlighted_metal = result
 
-#    assert highlighted_hbond_donor is not None
-#    assert highlighted_hbond_acceptor is not None
-#    assert highlighted_hbond_both is not None
-#    assert highlighted_hydrophobic is not None
-#    assert highlighted_waterbridge is not None
-#    assert highlighted_halogen is not None
-#    assert highlighted_ni is not None
-#    assert highlighted_pication is not None
+    assert highlighted_hbond_donor is not None
+    assert highlighted_hbond_acceptor is not None
+    assert highlighted_hbond_both is not None
+    assert highlighted_hydrophobic is not None
+    assert highlighted_waterbridge is not None
+    assert highlighted_halogen is not None
+    assert highlighted_ni is not None
+    assert highlighted_pication is not None
     
 def test_update_dict():
     # Test case 1: Check if the target dictionary is updated correctly
@@ -217,4 +223,3 @@ def test_arranged_figure_generation():
 # Run the tests
 if __name__ == '__main__':
     pytest.main()
-
