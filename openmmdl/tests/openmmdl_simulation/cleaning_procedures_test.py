c0c11c50669775082949c3b41f519126ac6a0edf
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2024-01-16 18:20:12 +0100

    Update cleaning_procedures_test.py

--- openmmdl/tests/openmmdl_simulation/cleaning_procedures_test.py
+++ openmmdl/tests/openmmdl_simulation/cleaning_procedures_test.py
@@ -84,24 +84,56 @@ def test_post_md_file_movement():
     protein_name = test_data_directory / '6b73.pdb'
     prmtop = test_data_directory / '6b73.prmtop'
     inpcrd = test_data_directory / '6b73.inpcrd'
+    protein_no_solvent = test_data_directory / 'prepared_no_solvent_6b73.pdb'
+    protein_solvent = test_data_directory / 'solvent_padding_6b73.pdb'
+    protein_equilibration = test_data_directory / 'Equilibration_6b73.pdb'
+    protein_minimization = test_data_directory / 'Energyminimization_6b73.pdb'
+    output_pdb = test_data_directory / 'output_6b73.pdb'
+    mdtraj_top = test_data_directory / 'centered_old_coordinates_top.pdb'
+    prot_lig_top = test_data_directory / 'prot_lig_top.pdb'
+    checkpoint = test_data_directory / 'checkpoint.chk'
+    checkpoint_10x = test_data_directory / '10x_checkpoint.chk'
     
     # Assert that the input files exist before moving
     assert os.path.exists(ligand)
     assert os.path.exists(protein_name)
     assert os.path.exists(prmtop)
     assert os.path.exists(inpcrd)
+    assert os.path.exists(protein_no_solvent)
+
+    shutil.copy(str(protein_no_solvent), '.')
+    shutil.copy(str(protein_solvent), '.')
+    shutil.copy(str(protein_equilibration), '.')
+    shutil.copy(str(protein_minimization), '.')
+    shutil.copy(str(output_pdb), '.')
+    shutil.copy(str(mdtraj_top), '.')
+    shutil.copy(str(prot_lig_top), '.')
+    shutil.copy(str(checkpoint), '.')
+    shutil.copy(str(checkpoint_10x), '.')
+    shutil.copy(str(protein_name), '.')
+    protein_name = '6b73.pdb'
 
     # Call the post_md_file_movement function
-    post_md_file_movement(protein_name, prmtop, inpcrd, ligand)
+    post_md_file_movement(str(protein_name), str(prmtop), str(inpcrd), [str(ligand)])
     
     # Check if the files have been organized and moved to the correct directories
     input_files_dir = Path("Input_Files")
+    md_files_dir = Path("MD_Files")
+    md_postprocessing_dir = Path("MD_Postprocessing")
+    final_output_dir = Path("Final_Output")
+    checkpoints_dir = Path("Checkpoints")
 
     assert os.path.exists(input_files_dir)
-    assert os.path.exists(input_files_dir / "6b73.pdb")
-    assert os.path.exists(input_files_dir / "6b73.prmtop")
-    assert os.path.exists(input_files_dir / "6b73.inpcrd")
-    assert os.path.exists(input_files_dir / "CVV.sdf")
+    assert os.path.exists(md_files_dir / "Pre_MD")
+    assert os.path.exists(md_files_dir / "Pre_MD" / "prepared_no_solvent_6b73.pdb")
+    assert os.path.exists(md_files_dir / "Pre_MD" / "solvent_padding_6b73.pdb")
+    assert os.path.exists(md_files_dir / "Minimization_Equilibration" / "Equilibration_6b73.pdb")
+    assert os.path.exists(md_files_dir / "Minimization_Equilibration" / "Energyminimization_6b73.pdb")
+    assert os.path.exists(md_files_dir / "MD_Output" / "output_6b73.pdb")
+    assert os.path.exists(md_postprocessing_dir / "centered_old_coordinates_top.pdb")
+    assert os.path.exists(final_output_dir / "Prot_Lig" / "prot_lig_top.pdb")
+    assert os.path.exists(checkpoints_dir / "checkpoint.chk")
+    assert os.path.exists(checkpoints_dir / "10x_checkpoint.chk")
 
 # Run the tests
 if __name__ == "__main__":
