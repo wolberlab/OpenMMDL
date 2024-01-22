ae912f984246b179ba7e3849c50f3169243350e4
Yu Chen <xixichennn@gmail.com>
2024-01-22 13:03:55 +0100

    generate the pdb file for each binding mode

--- openmmdl/_version.py
+++ openmmdl/_version.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-__version__ = "1.0.0+606.g6ff3138.dirty"
-=======
-__version__ = "1.0.0+617.gb2cee40.dirty"
->>>>>>> 09c467ba09a3ea4c4acc0cefb67a3e25971dffec
+__version__ = "1.0.0+648.gd517f79.dirty"
