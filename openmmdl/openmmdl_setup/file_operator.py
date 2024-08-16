acb74366b87760c2149c6131aae8d46817621c49
ARMI <armenianapoli@gmail.com>
2024-08-16 13:45:31 +0200

    Update file_operator.py
    type hinting and checking + run by black

--- openmmdl/openmmdl_setup/file_operator.py
+++ openmmdl/openmmdl_setup/file_operator.py
@@ -1,11 +1,19 @@
 import tempfile
 import shutil
 from werkzeug.utils import secure_filename
+from typing import Dict, List, Tuple, Any
+import tempfile
+import shutil
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
+
+from typing import Dict, List, Tuple
+import os
 
 
 class LigandExtractor:
     @staticmethod
-    def extract_ligand_name(lig_file_name):
+    def extract_ligand_name(lig_file_name: str) -> str:
         """
         Extracts the ligand name from the file name based on its extension.
 
@@ -24,6 +32,9 @@ class LigandExtractor:
             The extracted ligand name. For `.pdb` files, this is the file name without the `.pdb` extension.
             For `.sdf` files, it returns a default name `"UNL"`.
         """
+        if not isinstance(lig_file_name, str):
+            raise TypeError("lig_file_name must be a string")
+
         if lig_file_name.endswith(".sdf"):
             lig_name = "UNL"
         elif lig_file_name.endswith(".pdb"):
@@ -42,7 +53,9 @@ class FileUploader:
     """
 
     @staticmethod
-    def save_uploaded_files(uploadedFiles, request):
+    def save_uploaded_files(
+        uploadedFiles: Dict[str, List[Tuple[Any, str]]], request: Any
+    ) -> None:
         """
         Saves files from the request into temporary storage and updates the given dictionary.
 
@@ -58,10 +71,19 @@ class FileUploader:
         -------
         None
         """
+        if not isinstance(uploadedFiles, dict):
+            raise TypeError("uploadedFiles must be a dictionary")
+
+        if not hasattr(request, "files"):
+            raise TypeError("request object must have a 'files' attribute")
+
         uploadedFiles.clear()
         for key in request.files:
-            filelist = []
+            filelist: List[Tuple[Any, str]] = []
             for file in request.files.getlist(key):
+                if not isinstance(file, FileStorage):
+                    raise TypeError("file must be a FileStorage instance")
+
                 temp = tempfile.TemporaryFile()
                 shutil.copyfileobj(file, temp)
                 filelist.append((temp, secure_filename(file.filename)))
