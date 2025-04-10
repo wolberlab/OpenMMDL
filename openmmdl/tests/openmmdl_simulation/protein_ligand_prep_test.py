cc7227d657a8494154d73239fc7f6fcb734854c0
Niklas Piet Doering <itsnik12@yahoo.de>
2025-04-10 12:04:04 +0200

    fixed simulation test error

--- openmmdl/tests/openmmdl_simulation/protein_ligand_prep_test.py
+++ openmmdl/tests/openmmdl_simulation/protein_ligand_prep_test.py
@@ -101,6 +101,7 @@ forcefield = generate_forcefield(
     solvent_ff=water_selected,
     add_membrane=add_membrane,
     rdkit_mol=ligand_prepared,
+    smallMoleculeForceField="gaff"
 )
 complex_topology, complex_positions = merge_protein_and_ligand(protein_pdb, omm_ligand)
 modeller = app.Modeller(complex_topology, complex_positions)
