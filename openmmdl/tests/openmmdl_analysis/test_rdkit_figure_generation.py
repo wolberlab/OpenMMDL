12e08e4184759b421c05a114effd6dbfe9e9b19a
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2025-04-18 23:13:43 +0200

    Update test_rdkit_figure_generation.py
    test

--- openmmdl/tests/openmmdl_analysis/test_rdkit_figure_generation.py
+++ openmmdl/tests/openmmdl_analysis/test_rdkit_figure_generation.py
@@ -58,7 +58,8 @@ def test_highlight_numbers():
 
     starting_idx = 1  # Updated starting index
 
-    result = highlight_numbers(split_data, starting_idx)
+    interaction = InteractionProcessor(complex, lig_no_h)
+    result = interaction.highlight_numbers(split_data, starting_idx)
 
     (
         highlighted_hbond_donor,
@@ -87,22 +88,23 @@ def test_highlight_numbers():
 
 
 def test_update_dict():
+    interaction = InteractionProcessor(complex, lig_no_h)
     # Test case 1: Check if the target dictionary is updated correctly
     target_dict = {1: "1", 2: "2"}
     source_dict = {3: "3", 4: "4"}
-    update_dict(target_dict, source_dict)
+    interaction.update_dict(target_dict, source_dict)
     assert target_dict == {1: "1", 2: "2", 3: "3", 4: "4"}
 
     # Test case 2: Check if the function handles multiple source dictionaries
     target_dict = {}
     source_dict1 = {1: "1"}
     source_dict2 = {2: "2", 3: "3"}
-    update_dict(target_dict, source_dict1, source_dict2)
+    interaction.update_dict(target_dict, source_dict1, source_dict2)
     assert target_dict == {1: "1", 2: "2", 3: "3"}
 
     # Test case 3: Check if the function handles empty source dictionaries
     target_dict = {1: "1", 2: "2"}
-    update_dict(target_dict)  # No source dictionaries provided
+    interaction.update_dict(target_dict)  # No source dictionaries provided
     assert target_dict == {1: "1", 2: "2"}
 
 
