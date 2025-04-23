8e651fa37900f9228366c880122391ae7984ba17
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2025-04-24 01:03:49 +0200

    Update test_trajectory_saving.py

--- openmmdl/tests/openmmdl_analysis/test_trajectory_saving.py
+++ openmmdl/tests/openmmdl_analysis/test_trajectory_saving.py
@@ -16,3 +16,24 @@ def test_init():
     assert saver.ligname == "LIG"
     assert saver.special == "HEM"
     assert saver.nucleic is False
+
+def test_save_interacting_waters_trajectory(tmp_path):
+    pdb_md = mda.Universe(pdb_file, dcd_file)
+    saver = TrajectorySaver(pdb_md, "UNK", None, nucleic=False)
+    water_id = [7536]
+
+    out_dir = tmp_path / "output"
+    out_dir.mkdir()
+
+    saver.save_interacting_waters_trajectory(interacting_waters=water_id, outputpath=str(out_dir) + "/")
+
+    # Filepaths
+    pdb_out = out_dir / "interacting_waters.pdb"
+    dcd_out = out_dir / "interacting_waters.dcd"
+
+    # Assertions
+    assert pdb_out.exists(), "PDB file was not created."
+    assert dcd_out.exists(), "DCD file was not created."
+
+    assert pdb_out.stat().st_size > 0, "PDB file is empty."
+    assert dcd_out.stat().st_size > 0, "DCD file is empty."
