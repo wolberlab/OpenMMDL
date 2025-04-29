81e2675549f43a5eeb43293678327aba9c8d68e7
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2025-04-30 00:48:21 +0200

    Update rdkit_figure_generation.py
    moved image handeling to separate file and moved positions of classes

--- openmmdl/openmmdl_analysis/rdkit_figure_generation.py
+++ openmmdl/openmmdl_analysis/rdkit_figure_generation.py
@@ -1,95 +1,7 @@
-import matplotlib.pyplot as plt
-import rdkit
+import MDAnalysis as mda
 from rdkit import Chem
 from rdkit.Chem import AllChem, Draw
-from PIL import Image
 import cairosvg
-import pylab
-import os
-import MDAnalysis as mda
-
-
-class LigandImageGenerator:
-    def __init__(
-        self,
-        ligand_name,
-        complex_pdb_file,
-        ligand_no_h_pdb_file,
-        output_svg_filename,
-        fig_type="svg",
-    ):
-        """
-        Initialize the LigandImageGenerator class.
-
-        Args:
-            ligand_name (str): Name of the ligand in the protein-ligand complex topology.
-            complex_pdb_file (str): Path to the protein-ligand complex PDB file.
-            ligand_no_h_pdb_file (str): Path to the ligand PDB file without hydrogens.
-            output_svg_filename (str): Name of the output SVG file.
-            fig_type (str): Type of the output figure. Can be "svg" or "png".
-        """
-        self.ligand_name = ligand_name
-        self.complex_pdb_file = complex_pdb_file
-        self.ligand_no_h_pdb_file = ligand_no_h_pdb_file
-        self.output_svg_filename = output_svg_filename
-        self.fig_type = fig_type
-
-    def generate_image(self):
-        """Generates an SVG image (or PNG) of the ligand."""
-        try:
-            # Load complex and ligand structures
-            complex = mda.Universe(self.complex_pdb_file)
-            ligand_no_h = mda.Universe(self.ligand_no_h_pdb_file)
-            lig_noh = ligand_no_h.select_atoms("all")
-            complex_lig = complex.select_atoms(f"resname {self.ligand_name}")
-
-            # Application of RDKit Converter to obtain rdkit mol of ligand
-            lig_atoms = complex_lig.convert_to("RDKIT")
-            prepared_ligand = Chem.RemoveAllHs(lig_atoms)
-            
-            AllChem.Compute2DCoords(prepared_ligand)
-
-            # Map atom indices between ligand_no_h and complex
-            for atom in prepared_ligand.GetAtoms():
-                atom_index = atom.GetIdx()
-                for lig_atom in lig_noh:
-                    lig_index = lig_atom.index
-                    if atom_index == lig_index:
-                        lig_atom_name = lig_atom.name
-                        for comp_lig in complex_lig:
-                            comp_lig_name = comp_lig.name
-                            if lig_atom_name == comp_lig_name:
-                                num = int(comp_lig.id)
-                                atom.SetAtomMapNum(num)
-
-            # Generate an SVG image of the ligand
-            drawer = Draw.MolDraw2DSVG(5120, 3200)
-            drawer.drawOptions().addStereoAnnotation = (
-                True  # Add stereo information if available
-            )
-            drawer.DrawMolecule(prepared_ligand)
-
-            # Adjust font size in the SVG output using the FontSize method
-            font_size = drawer.FontSize()
-            drawer.SetFontSize(
-                font_size * 0.5
-            )  # You can adjust the multiplier as needed
-
-            drawer.FinishDrawing()
-            svg = drawer.GetDrawingText().replace("svg:", "")
-
-            # Save the SVG image to the specified output file
-            with open(self.output_svg_filename, "w") as f:
-                f.write(svg)
-
-            # Convert to PNG if requested
-            if self.fig_type == "png":
-                png_filename = self.output_svg_filename.replace(".svg", ".png")
-                cairosvg.svg2png(url=self.output_svg_filename, write_to=png_filename)
-                print(f"PNG image saved as: {png_filename}")
-
-        except Exception as e:
-            print(f"Error: {e}")
 
 
 class InteractionProcessor:
@@ -331,189 +243,84 @@ class InteractionProcessor:
                     target_dict[int_key] = value
 
 
-class ImageMerger:
+class LigandImageGenerator:
     def __init__(
-        self, binding_mode, occurrence_percent, split_data, merged_image_paths
+        self,
+        ligand_name,
+        complex_pdb_file,
+        ligand_no_h_pdb_file,
+        output_svg_filename,
+        fig_type="svg",
     ):
-        self.binding_mode = binding_mode
-        self.occurrence_percent = occurrence_percent
-        self.split_data = split_data
-        self.merged_image_paths = merged_image_paths
-
-    def create_and_merge_images(self):
-        """Create and merge images to generate a legend for binding modes.
-
-        Returns:
-            list: Paths to the merged images.
         """
-        # Create the main figure and axis
-        fig = pylab.figure()
-        ax = fig.add_subplot(111)
+        Initialize the LigandImageGenerator class.
 
-        # Data for the x-axis and random data for demonstration
-        x = range(10)
-        data_points = [pylab.randn(10) for _ in range(len(self.split_data))]
+        Args:
+            ligand_name (str): Name of the ligand in the protein-ligand complex topology.
+            complex_pdb_file (str): Path to the protein-ligand complex PDB file.
+            ligand_no_h_pdb_file (str): Path to the ligand PDB file without hydrogens.
+            output_svg_filename (str): Name of the output SVG file.
+            fig_type (str): Type of the output figure. Can be "svg" or "png".
+        """
+        self.ligand_name = ligand_name
+        self.complex_pdb_file = complex_pdb_file
+        self.ligand_no_h_pdb_file = ligand_no_h_pdb_file
+        self.output_svg_filename = output_svg_filename
+        self.fig_type = fig_type
 
-        # Plot lines on the same axis and collect them into a list
-        lines = []
-        filtered_split_data = [
-            entry for entry in self.split_data if "FRAME" not in entry
-        ]
-        for i, data in enumerate(filtered_split_data):
-            y = data_points[i]
-            label = data.split()[-1]
-            type = data.split()[-2]
-            if label == "hydrophobic":
-                (line,) = ax.plot(
-                    x, y, label=data, color=(1.0, 1.0, 0.0), linewidth=5.0
-                ) # yellow
-            elif label == "hbond":
-                if type == "Acceptor":
-                    (line,) = ax.plot(
-                        x, y, label=data, color=(1.0, 0.6, 0.6), linewidth=5.0
-                    ) # light red / pink
-                elif type == "Donor":
-                    (line,) = ax.plot(
-                        x, y, label=data, color=(0.3, 0.5, 1.0), linewidth=5.0
-                    ) # light blue
-            elif label == "halogen":
-                (line,) = ax.plot(
-                    x, y, label=data, color=(1.0, 0.0, 0.9), linewidth=5.0
-                ) # magenta / hot pink
-            elif label == "pistacking":
-                (line,) = ax.plot(
-                    x, y, label=data, color=(0.0, 0.0, 1.0), linewidth=5.0
-                ) # blue
-            elif label == "pication":
-                (line,) = ax.plot(
-                    x, y, label=data, color=(0.0, 0.0, 1.0), linewidth=5.0
-                ) # blue
-            elif label == "waterbridge":
-                (line,) = ax.plot(
-                    x, y, label=data, color=(0.0, 1.0, 0.9), linewidth=5.0
-                ) # cyan / aqua
-            elif label == "metal":
-                (line,) = ax.plot(
-                    x, y, label=data, color=(1.0, 0.6, 0.0), linewidth=5.0
-                ) # orange
-            elif label == "saltbridge":
-                if type == "NI":
-                    (line,) = ax.plot(
-                        x, y, label=data, color=(1.0, 0.6, 0.0), linewidth=5.0
-                    ) # orange
-                elif type == "PI":
-                    (line,) = ax.plot(
-                        x, y, label=data, color=(0.3, 0.9, 0.8), linewidth=5.0
-                    ) # turquoise / teal
-            else:
-                (line,) = ax.plot(x, y, label=data)
-            lines.append(line)
+    def generate_image(self):
+        """Generates an SVG image (or PNG) of the ligand."""
+        try:
+            # Load complex and ligand structures
+            complex = mda.Universe(self.complex_pdb_file)
+            ligand_no_h = mda.Universe(self.ligand_no_h_pdb_file)
+            lig_noh = ligand_no_h.select_atoms("all")
+            complex_lig = complex.select_atoms(f"resname {self.ligand_name}")
 
-        # Create a separate figure for the legend
-        figlegend = pylab.figure(figsize=(8, 6))
+            # Application of RDKit Converter to obtain rdkit mol of ligand
+            lig_atoms = complex_lig.convert_to("RDKIT")
+            prepared_ligand = Chem.RemoveAllHs(lig_atoms)
+            
+            AllChem.Compute2DCoords(prepared_ligand)
 
-        # Add a legend to the subplot (ax) using the lines and full entries as labels
-        legend = figlegend.legend(lines, filtered_split_data, loc="center")
+            # Map atom indices between ligand_no_h and complex
+            for atom in prepared_ligand.GetAtoms():
+                atom_index = atom.GetIdx()
+                for lig_atom in lig_noh:
+                    lig_index = lig_atom.index
+                    if atom_index == lig_index:
+                        lig_atom_name = lig_atom.name
+                        for comp_lig in complex_lig:
+                            comp_lig_name = comp_lig.name
+                            if lig_atom_name == comp_lig_name:
+                                num = int(comp_lig.id)
+                                atom.SetAtomMapNum(num)
 
-        # Set the linewidth of the legend lines to be thicker
-        for line in legend.get_lines():
-            line.set_linewidth(5.0)
+            # Generate an SVG image of the ligand
+            drawer = Draw.MolDraw2DSVG(5120, 3200)
+            drawer.drawOptions().addStereoAnnotation = (
+                True  # Add stereo information if available
+            )
+            drawer.DrawMolecule(prepared_ligand)
 
-        # Add text above the legend
-        figlegend.text(
-            0.5, 0.9, f"{self.binding_mode}", ha="center", fontsize=12, weight="bold"
-        )
-        figlegend.text(
-            0.5,
-            0.85,
-            f"Occurrence {self.occurrence_percent}%",
-            ha="center",
-            fontsize=12,
-            weight="bold",
-        )
+            # Adjust font size in the SVG output using the FontSize method
+            font_size = drawer.FontSize()
+            drawer.SetFontSize(
+                font_size * 0.5
+            )  # You can adjust the multiplier as needed
 
-        # Save the legend figure to a file
-        legend_filename = f"{self.binding_mode}_legend.png"
-        figlegend.savefig(legend_filename)
+            drawer.FinishDrawing()
+            svg = drawer.GetDrawingText().replace("svg:", "")
 
-        # Read the two images
-        image1 = Image.open(f"{self.binding_mode}.png")
-        image2 = Image.open(legend_filename)
+            # Save the SVG image to the specified output file
+            with open(self.output_svg_filename, "w") as f:
+                f.write(svg)
 
-        # Resize the first image
-        image1_size = image1.size
-        image2_size = image2.size
-        total_width = image1_size[0] + image2_size[0]
-        new_image = Image.new("RGB", (total_width, image1_size[1]))
-        new_image.paste(image1, (0, 0))
-        new_image.paste(image2, (image1_size[0], 0))
+            # Convert to PNG if requested
+            if self.fig_type == "png":
+                png_filename = self.output_svg_filename.replace(".svg", ".png")
+                cairosvg.svg2png(url=self.output_svg_filename, write_to=png_filename)
+                print(f"PNG image saved as: {png_filename}")
 
-        # Save the merged image
-        merged_image_filename = f"{self.binding_mode}_merged.png"
-        new_image.save(merged_image_filename, "PNG")
-
-        # Append the merged image path to the list
-        self.merged_image_paths.append(merged_image_filename)
-
-        # Remove the original files
-        os.remove(f"{self.binding_mode}.png")
-        os.remove(legend_filename)
-        os.remove(f"{self.binding_mode}.svg")
-
-        return self.merged_image_paths
-
-
-class FigureArranger:
-    def __init__(self, merged_image_paths, output_path):
-        self.merged_image_paths = merged_image_paths
-        self.output_path = output_path
-
-    def arranged_figure_generation(self):
-        """Generate an arranged figure by arranging merged images in rows and columns."""
-        # Open the list of images
-        merged_images = [Image.open(path) for path in self.merged_image_paths]
-
-        # Calculate the maximum width and height for the images
-        max_width = max(image.size[0] for image in merged_images)
-        max_height = max(image.size[1] for image in merged_images)
-
-        # Determine the number of images per row (in your case, 2 images per row)
-        images_per_row = 2
-
-        # Calculate the number of rows and columns required
-        num_rows = (len(merged_images) + images_per_row - 1) // images_per_row
-        total_width = max_width * images_per_row
-        total_height = max_height * num_rows
-
-        # Create a new image with the calculated width and height
-        big_figure = Image.new(
-            "RGB", (total_width, total_height), (255, 255, 255)
-        )  # Set background to white
-
-        x_offset = 0
-        y_offset = 0
-
-        for image in merged_images:
-            # Paste the image onto the big figure
-            big_figure.paste(image, (x_offset, y_offset))
-
-            # Update offsets
-            x_offset += max_width
-
-            # Move to the next row if necessary
-            if x_offset >= total_width:
-                x_offset = 0
-                y_offset += max_height
-
-        # Save the big figure
-        big_figure.save(self.output_path, "PNG")
-
-        # Rename the merged image
-        os.rename(
-            self.output_path,
-            "Binding_Modes_Markov_States/" + os.path.basename(self.output_path),
-        )
-
-        # Remove the individual image files
-        for path in self.merged_image_paths:
-            os.remove(path)
+        except Exception as e:
+            print(f"Error: {e}")
