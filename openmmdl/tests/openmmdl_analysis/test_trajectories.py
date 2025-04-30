1b943a1de5006701e1f80f4bcbc53b6928ca5dfc
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2025-04-30 13:59:51 +0200

    Update and rename test_trajectory_saving.py to test_trajectories.py

--- openmmdl/tests/openmmdl_analysis/test_trajectories.py
+++ openmmdl/tests/openmmdl_analysis/test_trajectories.py
@@ -3,7 +3,7 @@ from unittest.mock import MagicMock, patch
 import MDAnalysis as mda
 from pathlib import Path
 # Import the class to be tested
-from openmmdl.openmmdl_analysis.trajectory_saving import TrajectorySaver
+from openmmdl.openmmdl_analysis.trajectories import TrajectorySaver
 
 test_data_directory = Path("openmmdl/tests/data/in")
 pdb_file = test_data_directory / "interacting_waters.pdb"
