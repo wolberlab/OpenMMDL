b5175a6d52b8e765bda175c0116f3303c6de6b5a
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2025-04-30 18:04:00 +0200

    Update utils.py
    moved update_values

--- openmmdl/openmmdl_analysis/utils.py
+++ openmmdl/openmmdl_analysis/utils.py
@@ -28,3 +28,17 @@ def combine_subdict_values(data):
         combined_data["all"].extend(sub_dict.values())
 
     return combined_data
+
+
+def update_values(self, df, new, unique_data, row_name):
+    """Update the values in the input DataFrame based upon the frame values and an reference DataFrame.
+
+    Args:
+        df (pandas dataframe): Input DataFrame that will be updated.
+        new (pandas dataframe): The reference DataFrame containing values that are used to update the input DataFrame.
+        unique_data (dict): A dictionary containing keys that represent the specific unique column names that need to be updated in the input DataFrame.
+    """
+    for idx, row in df.iterrows():
+        frame_value = row[row_name]
+        values_to_update = new.loc[frame_value, list(unique_data.values())]
+        df.loc[idx, list(unique_data.values())] = values_to_update
