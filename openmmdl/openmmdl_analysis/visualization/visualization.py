690fe8f54ed467220474aef1a2c2c83a45be9dec
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2025-05-10 00:18:06 +0200

    Update visualization.py
    added data

--- openmmdl/openmmdl_analysis/visualization/visualization.py
+++ openmmdl/openmmdl_analysis/visualization/visualization.py
@@ -8,30 +8,68 @@ import shutil
 
 
 class Visualizer:
+    """
+    Visualizes the OpenMMDL Analysis output of the trajectories with annotated interaction clouds and water molecule interactions.
+
+    The class provides tools to render 3D molecular scenes using NGLview, including trajectory
+    visualization, ligand highlighting, interacting water molecules, and various interaction cloud types
+    (e.g., hydrogen bonds, hydrophobic regions, etc.).
+
+    Attributes
+    ----------
+    md : MDAnalysis.Universe
+        The loaded trajectory and topology data used for visualization.
+    cloud : dict
+        Dictionary parsed from the JSON file representing interaction clouds (e.g., hydrophobic, donor, etc.).
+    ligname : str
+        Ligand selection string used in NGLview for highlighting.
+    special : str
+        Additional selection string for visual emphasis (can be None).
+    """
     def __init__(self, md, cloud_path, ligname, special):
         self.md = md
-        self.cloud = self.load_cloud(cloud_path)
+        self.cloud = self._load_cloud(cloud_path)
         self.ligname = ligname
         self.special = special
 
-    def load_cloud(self, cloud_path):
+    def _load_cloud(self, cloud_path):
+        """
+        Loads interaction clouds from a JSON file.
+    
+        Parameters
+        ----------
+        cloud_path : str
+            Path to the JSON file containing the interaction clouds.
+    
+        Returns
+        -------
+        dict
+            Dictionary representing different types of interaction clouds and their properties.
+        """
         with open(cloud_path, "r") as f:
             data = json.load(f)
+
         return data
 
     def visualize(
         self, receptor_type="protein or nucleic", height="1000px", width="1000px"
     ):
-        """Generates visualization of the trajectory with the interacting waters and interaction clouds.
+        """
+        Generates visualization of the trajectory with the interacting waters and interaction clouds.
 
-        Args:
-            ligname (str): name of the ligand in the pdb file
-            receptor_type (str, optional): type of receptor. Defaults to 'protein or nucleic'.
-            height (str, optional): height of the visualization. Defaults to '1000px'.
-            width (str, optional): width of the visualization. Defaults to '1000px'.
+        Parameters
+        ----------
+        receptor_type : str, optional
+            Type of receptor. Defaults to 'protein or nucleic'.
+        height : str, optional
+            Height of the visualization. Defaults to '1000px'.
+        width : str, optional
+            Width of the visualization. Defaults to '1000px'.
 
-        Returns:
-            nglview widget: returns an nglview.widget object containing the visualization
+        Returns
+        -------
+        nglview.NGLWidget
+            Returns an nglview.widget object containing the visualization
         """
 
         sphere_buffers = []
@@ -82,11 +120,15 @@ class Visualizer:
             view._js(js)
         view.layout.width = width
         view.layout.height = height
+
         return view
 
 
 def run_visualization():
-    """Runs the visualization notebook in the current directory. The visualization notebook is copied from the package directory to the current directory and automaticaly started."""
+    """
+    Runs the visualization notebook in the current directory. 
+    The visualization notebook is copied from the package directory to the current directory and automaticaly started.
+    """
     package_dir = os.path.dirname(__file__)
     notebook_path = os.path.join(package_dir, "visualization.ipynb")
     current_dir = os.getcwd()
